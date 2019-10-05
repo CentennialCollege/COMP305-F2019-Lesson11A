849fcc67d5b10ade8add4f3027342b6ce9650f01
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-10-05 11:26:53 -0400

    Initial Commit

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class EnemyController : MonoBehaviour
+{
+    public Animator animator;
+    public SpriteRenderer spriteRender;
+
+    public EnemyAnimState enemyAnimState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyAnimState = EnemyAnimState.WALK;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Scripts/HeroController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Util;
+
+public class HeroController : MonoBehaviour
+{
+    public Animator animator;
+
+    public HeroAnimState heroAnimState;
+
+    public SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        heroAnimState = HeroAnimState.IDLE;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Idle
+        if(Input.GetAxis("Horizontal") == 0)
+        {
+            animator.SetInteger("AnimState", (int)HeroAnimState.IDLE);
+            heroAnimState = HeroAnimState.IDLE;
+        }
+
+
+        // moving to the right
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            animator.SetInteger("AnimState", (int)HeroAnimState.WALK);
+            heroAnimState = HeroAnimState.WALK;
+            spriteRenderer.flipX = false;
+        }
+
+        // moving to the left
+        if(Input.GetAxis("Horizontal") < 0)
+        {
+            animator.SetInteger("AnimState", (int)HeroAnimState.WALK);
+            heroAnimState = HeroAnimState.WALK;
+            spriteRenderer.flipX = true;
+        }
+
+        // jumping
+        if(Input.GetAxis("Jump") > 0)
+        {
+            animator.SetInteger("AnimState", (int)HeroAnimState.JUMP);
+            heroAnimState = HeroAnimState.JUMP;
+        }
+
+        // not jumping
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetInteger("AnimState", (int)HeroAnimState.IDLE);
+            heroAnimState = HeroAnimState.IDLE;
+        }
+    }
+}
