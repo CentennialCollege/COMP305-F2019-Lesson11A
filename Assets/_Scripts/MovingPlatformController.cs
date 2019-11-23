5fc43758a59d3d8a7e901ed2dd5566a60b42c9b7
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2019-11-23 09:27:19 -0500

    added moving platform

---
+++ Assets/_Scripts/MovingPlatformController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformController : MonoBehaviour
+{
+    public float speedFactor = 2.5f;
+    public float height = 6.0f;
+    public bool isMoving = false;
+
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isMoving)
+        {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        timer += Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, Mathf.PingPong(timer, height) * speedFactor, 0.0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isMoving = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isMoving = false;
+        }
+    }
+
+}
